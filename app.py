7781b23747f711975f373bc55e925711a364c86b
Chong Xin Yi <ellyc1130@gmail.com>
2023-09-22 20:09:07 +0800

    Update

--- app.py
+++ app.py
@@ -32,42 +32,101 @@ def about():
 def student():
     return render_template('student.html')
 
+# Route to admin home page
+@app.route("/admin/<admin_id>")
+def admin(admin_id):
+    return render_template('admin.html', admin_id=admin_id)
+
+# Route to admin login page
 @app.route("/adminLogin")
 def adminLogin(msg=""):
     return render_template('adminLogin.html', msg=msg)
 
-@app.route("/login", methods=['GET'])
+# Route to admin profile page
+@app.route("/adminProfile/<admin_id>")
+def adminProfile(admin_id):
+    # Connect to MySQL database
+    cursor = db_conn.cursor()
+
+    try:
+        # Get admin data from database
+        cursor.execute('SELECT * FROM admin WHERE id = %s', (admin_id,))
+        account = cursor.fetchone() # If account not exists, account = None
+    finally:
+        cursor.close()
+
+    print(account[0])
+
+    return render_template('adminProfile.html', admin_id=admin_id, account=account)
+
+# Route to supervisor list page
+@app.route("/admin/supervisorList", methods=['GET'])
+def supervisorList():
+    # Connect to MySQL database
+    cursor = db_conn.cursor()
+
+    try:
+        # Get all supervisor data from database
+        cursor.execute('SELECT * FROM supervisor')
+        supervisors = cursor.fetchall()
+    finally:
+        cursor.close()
+
+    return render_template('supervisorList.html', supervisors=supervisors)
+
+# Admin edit profile function
+@app.route("/editProfile/<admin_id>")
+def editProfile(admin_id):
+    print(admin_id);
+
+    # Get user input name, email and phone number from HTML form
+    name = request.args.get('name')
+    email = request.args.get('email')
+    phone = request.args.get('phone')
+
+    # Connect to MySQL database
+    cursor = db_conn.cursor()
+
+    try:
+        # Update admin data in database
+        cursor.execute('UPDATE admin SET name = %s, email = %s, phone_number = %s WHERE id = %s', (name, email, phone, admin_id))
+        db_conn.commit()
+    finally:
+        cursor.close()
+
+    return redirect(url_for('adminProfile', admin_id=admin_id))
+
+# Admin login function
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     # Get user input email and password from HTML form
-    email = request.args.get('email')
+    admin_id = request.args.get('admin_id')
     password = request.args.get('password')
 
-    # Console log for debugging
-    print(email)
-    print(password)
-
-    # Check if email exists in accounts table in out database
+    # Connect to MySQL database
     cursor = db_conn.cursor()
-    cursor.execute('SELECT * FROM admin WHERE email = %s', (email,))
-    account = cursor.fetchone()
-
-    # Console log for debugging
-    print(account) # If account not exists, account = None
+    
+    try:
+        # Check if email exists in accounts table in out database
+        cursor.execute('SELECT * FROM admin WHERE id = %s', (admin_id,))
+        account = cursor.fetchone() # If account not exists, account = None
+    finally:
+        cursor.close()
 
     # If account exists in accounts table in out database
     if account:
         # Check if password correct
-        if password == account[2]:
+        if password == account[4]:
             # If password correct, redirect to admin page
-            return redirect(url_for('admin'))
+            return redirect(url_for('admin', admin_id=admin_id))
         else:
             # If password incorrect, redirect to admin login page with error message
             msg = 'Account exists but password incorrect'
-            return redirect(url_for('adminLogin', msg=msg))
+            return render_template('adminLogin.html', msg=msg)
     # If account not exists in accounts table in out database
     else:
         msg = 'Account does not exists'
-        return redirect(url_for('adminLogin', msg=msg))
+        return render_template('adminLogin.html', msg=msg)
     
 
 @app.route("/xy")
@@ -89,59 +148,6 @@ def jtPortfolio():
 @app.route("/yk")
 def ykPortfolio():
     return render_template('yk-portfolio.html')
-
-
-# @app.route("/addemp", methods=['POST'])
-# def AddEmp():
-    emp_id = request.form['emp_id']
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    pri_skill = request.form['pri_skill']
-    location = request.form['location']
-    emp_image_file = request.files['emp_image_file']
-
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
-    cursor = db_conn.cursor()
-
-    if emp_image_file.filename == "":
-        return "Please select a file"
-
-    try:
-
-        cursor.execute(insert_sql, (emp_id, first_name,
-                       last_name, pri_skill, location))
-        db_conn.commit()
-        emp_name = "" + first_name + " " + last_name
-        # Uplaod image file in S3 #
-        # emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-        # s3 = boto3.resource('s3')
-
-        # try:
-        #     print("Data inserted in MySQL RDS... uploading image to S3...")
-        #     s3.Bucket(custombucket).put_object(
-        #         Key=emp_image_file_name_in_s3, Body=emp_image_file)
-        #     bucket_location = boto3.client(
-        #         's3').get_bucket_location(Bucket=custombucket)
-        #     s3_location = (bucket_location['LocationConstraint'])
-
-        #     if s3_location is None:
-        #         s3_location = ''
-        #     else:
-        #         s3_location = '-' + s3_location
-
-        #     object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-        #         s3_location,
-        #         custombucket,
-        #         emp_image_file_name_in_s3)
-
-        # except Exception as e:
-        #     return str(e)
-
-    finally:
-        cursor.close()
-
-    print("all modification done...")
-    return render_template('AddEmpOutput.html', name=emp_name)
 
 
 if __name__ == '__main__':
